3923f516ebec6e37af31e423b4390c4c9d0aac1b
matt969696 <matsce2020@gmail.com>
2020-12-15 15:35:28 +0000

    feat() : initial solution for 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -13,16 +13,18 @@ void print_python_list_info(PyObject *p)
 	int i;
 	PyObject *item;
 
-	size = (int)PyList_Size(p);
-	alloc = ((PyListObject *)p)->allocated;
-
-	printf("[*] Size of the Python List = %i\n", size);
-	printf("[*] Allocated =  %i\n", alloc);
-
-	for (i = 0; i < size; i++)
+	if (PyList_Check(p))
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %i: %s\n", i, Py_TYPE(item)->tp_name);
-	}
+		size = (int)PyList_Size(p);
+		alloc = ((PyListObject *)p)->allocated;
 
+		printf("[*] Size of the Python List = %i\n", size);
+		printf("[*] Allocated = %i\n", alloc);
+
+		for (i = 0; i < size; i++)
+		{
+			item = PyList_GetItem(p, i);
+			printf("Element %i: %s\n", i, Py_TYPE(item)->tp_name);
+		}
+	}
 }
