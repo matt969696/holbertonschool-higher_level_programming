49f29907a1f04e767c2d0fba2b19961ed7d66eb9
matt969696 <matsce2020@gmail.com>
2020-12-17 14:06:38 +0000

    feat() : add initial solution for 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,9 @@ def roman_to_int(roman_string):
     dic1 = dict(M=1000, D=500, C=100, L=50, X=10, V=5, I=1)
     dic2 = dict(CM=900, CD=400, XC=90, XL=40, IX=9, IV=4)
 
+    if str is None:
+        return 0
+
     if isinstance(roman_string, str):
         if roman_string == "":
             return 0
