4ad1ed9ee3477b038b33b20e4e2813bf4fbee81b
matt969696 <matsce2020@gmail.com>
2020-12-17 16:18:11 +0000

    fix() : new solution 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x*x, vec)) for vec in matrix]
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
