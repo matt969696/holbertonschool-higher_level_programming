01c1f70dd3785173915c132c4daad3d46934aec1
matt969696 <matsce2020@gmail.com>
2021-01-04 11:31:40 +0000

    fix() : correct typo in message

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except:
         quotient = None
     finally:
-        print("Inside result : {}".format(quotient))
+        print("Inside result: {}".format(quotient))
     return quotient
