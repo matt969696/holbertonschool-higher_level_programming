8a2eadfac16eaf189715a4f7ca9ae164ab7b2994
matt969696 <matsce2020@gmail.com>
2021-01-05 15:25:10 +0000

    feat() : Initial solution for 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -76,6 +76,8 @@ class SinglyLinkedList:
         """
         out = ""
         cur = self.__head
+        if cur is None:
+            return '\n'
         while cur is not None:
             out = out + str(cur.data)
             cur = cur.next_node
