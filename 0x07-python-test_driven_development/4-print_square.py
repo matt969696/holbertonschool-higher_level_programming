158214002dcbd583af89cda64277044679c28f48
matt969696 <matsce2020@gmail.com>
2021-01-08 17:04:55 +0000

    fix() : correct Error type for 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,7 +15,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
 
     if size < 0:
-        raise TypeError("size must be >= 0")
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         print("#" * size)
