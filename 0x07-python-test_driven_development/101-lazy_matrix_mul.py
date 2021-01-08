7cb3d35d25b15eb7ececeb102ffc753b2013cf09
matt969696 <matsce2020@gmail.com>
2021-01-08 15:45:03 +0000

    fix() : pep8 for 101

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,13 +4,14 @@
 """
 
 from numpy import dot
+
+
 def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies 2 matrices
     Return the new matrix
-    The 2 matrix must be non null list of list
     The 2 matrix must be multiable
     All elements of the 2 matrix must be integers or floats
     """
-
+    outmat = dot(m_a, m_b)
     return (dot(m_a, m_b))
