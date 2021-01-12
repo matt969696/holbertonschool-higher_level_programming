c4a749c4556fc8054611acadec12e9d2af01843b
matt969696 <matsce2020@gmail.com>
2021-01-12 18:43:45 +0000

    fix() : fix solution for 31 & 33

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __init__(self):
-        pass
-    __slots__ = ('first_name')
+    """ Simple implementation of a simple Class """
+    __slots__ = ['first_name']
