728bd467a3887732441eaf85f5c2287b7ddac5b3
matt969696 <matsce2020@gmail.com>
2021-01-19 17:34:22 +0000

    fix() : added extra lines

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 """
-Module that contains 2 simple classes : BaseGeometry and Rectangle
+Module that contains 1 simple classes : Rectangle
 """
 
 
-class BaseGeometry:
-    """Simple empty class """
-
-    def area(self):
-        """Raises an Exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method that validates that value is a positive integer"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
