a5d25d0e8b3b8c9e7aa5c98ec31670c6b12bcaf4
matt969696 <matsce2020@gmail.com>
2021-01-19 17:24:09 +0000

    fix() : added extra lines

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -44,5 +44,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """ Init method for Square class"""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
