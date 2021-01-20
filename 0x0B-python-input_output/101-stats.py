692a50177f58459403d4db646daf8ee13aaec3ce
matt969696 <matsce2020@gmail.com>
2021-01-20 20:04:41 +0000

    fix() : add test if code is not valid in 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     """ Code to retrieve and display datas"""
     size = 0
-    statusdict = {}
+    statdict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     try:
         nbline = 0
         for line in sys.stdin:
@@ -16,24 +16,25 @@ if __name__ == "__main__":
                     continue
                 nbline += 1
                 size += int(linetok[8])
-                stat = linetok[7]
-                if stat in statusdict:
-                    statusdict[stat] += 1
-                else:
-                    statusdict[stat] = 1
+                stat = int(linetok[7])
+                if stat in statdict:
+                    statdict[stat] += 1
             except:
                 pass
 
             if nbline % 10 == 0:
                 print("File size: {}".format(size))
-                for key, value in sorted(statusdict.items()):
-                    print("{}: {}".format(key, value))
+                for key, value in sorted(statdict.items()):
+                    if value != 0:
+                        print("{}: {}".format(key, value))
 
     except KeyboardInterrupt:
         print("File size: {}".format(size))
-        for key, value in sorted(statusdict.items()):
-            print("{}: {}".format(key, value))
+        for key, value in sorted(statdict.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
             raise
     print("File size: {}".format(size))
-    for key, value in sorted(statusdict.items()):
-        print("{}: {}".format(key, value))
+    for key, value in sorted(statdict.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
