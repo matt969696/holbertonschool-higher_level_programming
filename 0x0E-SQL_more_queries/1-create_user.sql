2be317712693e2e42278e3bc3e17287494e75377
matt969696 <matsce2020@gmail.com>
2021-03-02 12:33:48 +0000

    fix() : correct typo

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- creates the MySQL server
 -- user user_0d_1
-DROP USER user_0d_1@localhost;
-FLUSH PRIVILEGES;
-CREATE USER user_0d_1@localhost IDENTIFIED by 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 -- that can be found in the database hbtn_0d_usa
 SELECT id, name
 FROM cities
-WHERE state_id in (SELECT id from states WHERE name = 'California')
+WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
 ORDER BY id;
