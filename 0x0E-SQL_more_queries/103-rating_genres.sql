2f6c0406206003687762f65dd4fb81bae1caa018
matt969696 <matsce2020@gmail.com>
2021-03-02 13:26:21 +0000

    feat() : correct 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,7 @@
 -- by their rating
 SELECT a.name, sum(c.rate) AS rating
 FROM tv_genres a
-LEFT JOIN tv_show_genres b ON a.id = b.genre_id
-LEFT JOIN tv_show_ratings c ON b.show_id = c.show_id
+INNER JOIN tv_show_genres b ON a.id = b.genre_id
+INNER JOIN tv_show_ratings c ON b.show_id = c.show_id
 GROUP BY a.name
 ORDER BY rating DESC;
