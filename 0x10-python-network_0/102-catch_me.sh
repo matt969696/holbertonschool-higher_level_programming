4597a6cee3bcdfe9fd748a192f7d9dae15ee1a91
matt969696 <matsce2020@gmail.com>
2021-04-08 13:26:55 +0000

    fix() : 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catched or not catched
-curl -LX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
