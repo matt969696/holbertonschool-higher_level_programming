50878d8b7f7cdd56ddb204575c9062819400a51a
matt969696 <matsce2020@gmail.com>
2021-04-09 15:36:28 +0000

    fix() : fix for 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """uses the GitHub API to display your id"""
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1],
-                                                              sys.argv[2])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
+                                                              sys.argv[1])
     json = requests.get(url).json()
     for i in range(len(json)):
         myj = json[i]
